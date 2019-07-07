2b985ab4fc006ea95f0c360a2b2a62d1f8b118fc
Yogesh Kumar <kumarryogeshh@gmail.com>
2019-07-08 00:44:13 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <main>
+      <div className="intro">Hello, I'm Yogesh Kumar.</div>
+      <div className="tagline">
+        Solution Architect | Full Stack Dev | Serverless Enthusiast | Cloud
+        Fanatic
+      </div>
+      <div className="about">
+        I have a Masters Degree in Computer Applications and React Nanodegree
+        from Udacity. I am passionate about building high performance, scalable
+        and resilient software systems using opensource technologies and have
+        good experience at making production ready applications. I{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          href="http://ibosky.blogspot.com/"
         >
-          Learn React
+          blog
+        </a>{" "}
+        my learnings sometimes. My most neglected hobbies are photography and
+        music.
+      </div>
+      <br />
+      <div className="icons-social">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/yogeshhkumarr"
+        >
+          <i className="fab fa-linkedin" />
         </a>
-      </header>
-    </div>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/kumarryogeshh"
+        >
+          <i className="fab fa-github" />
+        </a>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://medium.com/@yogeshkumarr"
+        >
+          <i className="fab fa-medium" />
+        </a>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/igoy_k"
+        >
+          <i className="fab fa-twitter" />
+        </a>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://stackoverflow.com/users/1022003/yogeshhkumarr"
+        >
+          <i className="fab fa-stack-overflow" />
+        </a>
+      </div>
+    </main>
   );
-}
+};
 
 export default App;
